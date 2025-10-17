e127478f32777efb0417f963deca3e1e5a21ccaa
Calum Murray <cmurray@redhat.com>
2025-10-17 11:49:58 -0400

    feat: create agent spec
    Signed-off-by: Calum Murray <cmurray@redhat.com>

---
+++ pkg/agent/spec.go
@@ -0,0 +1,90 @@
+package agent
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"sigs.k8s.io/yaml"
+)
+
+const (
+	KindAgent = "Agent"
+)
+
+type AgentSpec struct {
+	Metadata AgentMetadata `json:"metadata"`
+	Commands AgentCommands `json:"commands"`
+}
+
+type AgentMetadata struct {
+	// Name of the agent
+	Name string `json:"name"`
+
+	// Version of the agent - used if Commads.GetVersion is not set
+	Version *string `json:"version,omitempty"`
+}
+
+type AgentCommands struct {
+	// Whether or not to create a virtual $HOME for executing the agent without existing config
+	UseVirtualHome bool `json:"useVirtualHome"`
+
+	// A template for how the mcp servers config files should be provided to the prompt
+	// the server file will be in {{ .File }}
+	ArgTemplateMcpServer string `json:"argTemplateMcpServer"`
+
+	// A template for how the mcp agents allowed tools should be provided to the prompt
+	// the server name will be in {{ .ServerName }}
+	// the tool name will be in {{ .ToolName }}
+	ArgTemplateAllowedTools string `json:"argTemplateAllowedTools"`
+
+	// A template command to run the agent with a prompt and some mcp servers
+	// the prompt will be in {{ .Prompt }}
+	// the servers will be in {{ .McpServerFileArgs }}
+	// the allowed tools will be in {{ .AllowedTools }}
+	RunPrompt string `json:"runPrompt"`
+
+	// An optional command to get the version of the agent
+	// useful for generic agents such as claude code that may autoupdate/have different versions on different machines
+	GetVersion *string `json:"getVersion,omitempty"`
+}
+
+func (a *AgentSpec) UnmarshalJSON(data []byte) error {
+	type Doppleganger AgentSpec
+	tmp := struct {
+		Kind string `json:"kind"`
+		*Doppleganger
+	}{
+		Doppleganger: (*Doppleganger)(a),
+	}
+
+	if err := json.Unmarshal(data, &tmp); err != nil {
+		return err
+	}
+
+	if tmp.Kind != KindAgent {
+		return fmt.Errorf("cannot decode kind '%s' as kind 'agent'", tmp.Kind)
+	}
+
+	return nil
+}
+
+func Read(data []byte) (*AgentSpec, error) {
+	spec := &AgentSpec{}
+
+	err := yaml.Unmarshal(data, spec)
+	if err != nil {
+		return nil, err
+	}
+
+	return spec, nil
+}
+
+func FromFile(path string) (*AgentSpec, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file '%s' for agentspec: %w", path, err)
+	}
+
+	return Read(data)
+}
